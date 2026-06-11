406e082dd208a227345372190069e96271731c47
chathura <chathurakavindu15@gmail.com>
2026-06-11 12:33:58 +0530

    Initialize Spring Boot project with Maven configuration, basic structure, and PostgreSQL setup. Include `.gitignore` and `.gitattributes` files.

---
+++ src/main/java/com/zeylex/denguesense/DenguesenseApplication.java
@@ -0,0 +1,13 @@
+package com.zeylex.denguesense;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DenguesenseApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DenguesenseApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/zeylex/denguesense/DenguesenseApplicationTests.java
@@ -0,0 +1,13 @@
+package com.zeylex.denguesense;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DenguesenseApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
