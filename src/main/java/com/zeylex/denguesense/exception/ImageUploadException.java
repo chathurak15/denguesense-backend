5f6538f0961e8d2e1501944b2fc7a245005dc2ba
Chathura Kavindu Bandara <chathurakavindu15@gmail.com>
2026-07-23 10:22:00 +0530

    feat: integrate Cloudinary image upload for breeding-site photos

---
+++ src/main/java/com/zeylex/denguesense/exception/ImageUploadException.java
@@ -0,0 +1,10 @@
+package com.zeylex.denguesense.exception;
+
+public class ImageUploadException extends RuntimeException {
+    public ImageUploadException(String message) {
+        super(message);
+    }
+    public ImageUploadException(String message, Throwable cause) {
+        super(message, cause);
+    }
+}
